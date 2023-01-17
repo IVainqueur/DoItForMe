86f4fdb2c6dda8620d4ccae21f0dcab76330377c
IVainqueur <ishimvainqueur@gmail.com>
2023-01-17 14:04:28 +0200

    [fix] working on the datetime issue

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const { App, subtype } = require('@slack/bolt');
 
 require('dotenv').config();
 
+const NIGEN_URI = process.env.NIGEN_URI
+
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -12,26 +14,56 @@ const app = new App({
 
 
 (async () => {
-    // Start your app
     await app.start(process.env.PORT || 3000);
-
     console.log('⚡️ Bolt app is running!');
 })();
 
-app.message(/hello|hi|hey/, async ({message, say}) => {
-    console.log("RECEIVED MESSAGE", message)
+app.message(/(hello|hi|hey) @doitforme/i, async ({ message, say }) => {
     await say(`Wassup, <@${message.user}>`);
 })
 
-// Snitching on edited messages
-app.message(subtype('message_changed'), ({message, say}) => {
-    console.log("RECEIVED EDITED MESSAGE", message)
-    say(`@channel, <@${message.message.user}> edited their message!\nFrom: ${message.previous_message.text}\nTo: ${message.message.text}`)
+// Snitching on deleted messages
+app.message(subtype('message_deleted'), async ({ message, say }) => {
+    const user = await getUserInfo(message.previous_message.user)
+    const userInfo = {
+        name: user ? user.profile.display_name : undefined,
+        avatar: user ? user.profile.image_72 : undefined,
+        id: user ? user.id : undefined,
+        locale: user ? user.locale : undefined,
+    }
+    console.log({tz: user.tz, message_ts: message.previous_message.ts})
+    const compositionDate = new Date(Number(Math.floor(message.previous_message.ts) * 1000)).toLocaleString('en-US', {
+        timeZone: user.tz,
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true
+    })
+
+    const thread_ts = message.previous_message.thread_ts ?? message.previous_message.ts
+
+    const reply = await say({
+        text: `<!channel>, <@${message.previous_message.user}> deleted their message!`,
+        thread_ts,
+    })
+
+
+    const screenshotURI = `${NIGEN_URI}?message=${encodeURI(message.previous_message.text)}&username=${encodeURI(userInfo.name)}&time=${encodeURI(compositionDate)}&avatar=${userInfo.avatar}`
+    await say({
+        text: ``,
+        thread_ts: reply.ts,
+        attachments: [
+            {
+                fallback: "Removed message info",
+                image_url: screenshotURI,
+                thumb_url: screenshotURI,
+                text: 'screenshot',
+
+            }
+        ]
+    })
+    console.log(screenshotURI)
 })
 
-// Snitching on deleted messages
-app.message(subtype('message_deleted'), async ({message, say}) => {
-    console.log("DELETED MESSAGE", message)
-    const reply = await say(`<!channel>, <@${message.previous_message.user}> deleted their message!`)
-    await say({text: `<@${message.previous_message.user}> had said\n*${message.previous_message.text}*`, thread_ts: reply.ts})
-})
+async function getUserInfo(userId) {
+    return (await app.client.users.info({ user: userId })).user ?? null
+}
